a8e9e7a4c76015d9da2308b48d79767493394efe
Zhu-junwei <1286630326@qq.com>
2021-06-17 16:26:28 +0800

    提取优化获取容器的方法

--- day02_eesy_02account_xmlioc/src/test/java/com/zjw/test/AccountServiceTest.java
+++ day02_eesy_02account_xmlioc/src/test/java/com/zjw/test/AccountServiceTest.java
@@ -2,6 +2,7 @@ package com.zjw.test;
 
 import com.zjw.domain.Account;
 import com.zjw.service.IAccountService;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,15 +14,24 @@ import java.util.List;
  */
 public class AccountServiceTest {
 
+    private ApplicationContext ac ;
+
+    /**
+     * 1. 初始化容器
+     */
+    @Before
+    public void init(){
+        ac = new ClassPathXmlApplicationContext("bean.xml");
+    }
+
     @Test
     public void testFindAll(){
-        //1、获取容器
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2、得到业务层对象
         IAccountService accountService = ac.getBean("accountService", IAccountService.class);
 
         //执行方法
         List<Account> accountList = accountService.findAllAccount();
+        testFindAccountById();
         for (Account account : accountList) {
             System.out.println(account);
         }
@@ -30,8 +40,6 @@ public class AccountServiceTest {
 
     @Test
     public void testFindAccountById(){
-        //1、获取容器
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2、得到业务层对象
         IAccountService accountService = ac.getBean("accountService", IAccountService.class);
         Account account = accountService.findAccountById(1);
@@ -41,14 +49,12 @@ public class AccountServiceTest {
 
     @Test
     public void testSaveAccount(){
-        //1、获取容器
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2、得到业务层对象
         IAccountService accountService = ac.getBean("accountService", IAccountService.class);
 
         Account account = new Account();
-        account.setId(4);
-        account.setName("zzz");
+        account.setId(7);
+        account.setName("abc");
         account.setMoney(9999f);
         accountService.saveAccount(account);
 
@@ -56,11 +62,9 @@ public class AccountServiceTest {
 
     @Test
     public void testUpdateAccount(){
-        //1、获取容器
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2、得到业务层对象
         IAccountService accountService = ac.getBean("accountService", IAccountService.class);
-        Account account = accountService.findAccountById(4);
+        Account account = accountService.findAccountById(1);
         System.out.println(account);
         account.setMoney(888F);
         accountService.updateAccount(account);
@@ -69,11 +73,9 @@ public class AccountServiceTest {
 
     @Test
     public void testDeleteAccount(){
-        //1、获取容器
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2、得到业务层对象
         IAccountService accountService = ac.getBean("accountService", IAccountService.class);
-        accountService.deleteAccount(5);
+        accountService.deleteAccount(7);
 
     }
 }
